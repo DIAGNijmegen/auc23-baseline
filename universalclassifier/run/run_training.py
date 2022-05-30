097785d7b1c41c4752942aa60a87106f9baea3f9
luukboulogne <luuk.boulogne@radboudumc.nl>
2022-05-30 09:53:27 +0200

    commented out trainer.validate since implementation is not complete yet

--- universalclassifier/run/run_training.py
+++ universalclassifier/run/run_training.py
@@ -116,10 +116,10 @@ def run_training(args):
 
         trainer.network.eval()
 
-        # predict validation
-        trainer.validate(save_softmax=args.npz, validation_folder_name=val_folder,
-                         run_postprocessing_on_folds=not disable_postprocessing_on_folds,
-                         overwrite=args.val_disable_overwrite)
+        # predict validation TODO: finish implementation of trainer.validate
+        #trainer.validate(save_softmax=args.npz, validation_folder_name=val_folder,
+        #                 run_postprocessing_on_folds=not disable_postprocessing_on_folds,
+        #                 overwrite=args.val_disable_overwrite)
 
         if network == '3d_lowres' and not args.disable_next_stage_pred:
             print("predicting segmentations for the next stage of the cascade")
