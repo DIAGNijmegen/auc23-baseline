74a5939294c9c7fa3c166d5da726bce3113ae308
Luuk Boulogne <50451985+luukboulogne@users.noreply.github.com>
2022-07-08 13:23:05 +0200

    Adding -u to python command

--- uc_train_on_sol.sh
+++ uc_train_on_sol.sh
@@ -17,9 +17,9 @@ export nnUNet_preprocessed="/preprocessed"
 export RESULTS_FOLDER="$1/trained_models"
 #export nnUNet_n_proc_DA=4
 
-echo running "python3 universalclassifier/sol/copy_preprocessed.py $4 $1/preprocessed /preprocessed"
-python3 universalclassifier/sol/copy_preprocessed.py $4 $1/preprocessed /preprocessed  # $4 is the task name or task ID
+echo running "python3 -u universalclassifier/sol/copy_preprocessed.py $4 $1/preprocessed /preprocessed"
+python3 -u universalclassifier/sol/copy_preprocessed.py $4 $1/preprocessed /preprocessed  # $4 is the task name or task ID
 mkdir -p "$1/trained_models"
 
-echo running "python3 uc_train.py ${@:2}"
-python3 uc_train.py ${@:2}
+echo running "python3 -u uc_train.py ${@:2}"
+python3 -u uc_train.py ${@:2}
