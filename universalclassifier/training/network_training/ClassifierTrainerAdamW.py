bf377c62bca7793c672b845f9ee0a7b2ec3dd729
luukboulogne <luuk.boulogne@radboudumc.nl>
2022-08-08 10:47:26 +0200

    bugfixes

--- universalclassifier/training/network_training/ClassifierTrainerAdamW.py
+++ universalclassifier/training/network_training/ClassifierTrainerAdamW.py
@@ -1,11 +1,13 @@
 # Copied and edited from https://github.com/MIC-DKFZ/nnUNet/blob/8e0ad8ebe0b24165419d087d4451b4631f5b37f2/nnunet/training/network_training/nnUNetTrainer.py#L432
 
-from ClassifierTrainer import ClassifierTrainer
+from universalclassifier.training.network_training.ClassifierTrainer import ClassifierTrainer
 import torch.optim
 
+
 class ClassifierTrainerAdamW(ClassifierTrainer):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, stage=None, unpack_data=True,
+                 deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, stage, unpack_data, deterministic, fp16)
 
     def initialize_optimizer_and_scheduler(self):
         assert self.network is not None, "self.initialize_network must be called first"
--- universalclassifier/training/network_training/ClassifierTrainerBestCheckpoint.py
+++ universalclassifier/training/network_training/ClassifierTrainerBestCheckpoint.py
@@ -1,28 +1,30 @@
-from ClassifierTrainer import ClassifierTrainer
+from universalclassifier.training.network_training.ClassifierTrainer import ClassifierTrainer
 import torch
 from nnunet.utilities.nd_softmax import softmax_helper
 from sklearn.metrics import roc_auc_score
+import numpy as np
 
 
 class ClassifierTrainerBestCheckpoint(ClassifierTrainer):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, stage=None, unpack_data=True,
+                 deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, stage, unpack_data, deterministic, fp16)
         self.online_eval_outputs_softmax = []
         self.online_eval_targets = []
 
     def run_online_evaluation(self, outputs, targets):
         with torch.no_grad():
             outputs_softmax = [softmax_helper(output) for output in outputs]
-            if len(self.online_eval_preds) == 0:
+            if len(self.online_eval_outputs_softmax) == 0:
                 assert len(self.online_eval_targets) == 0
-                self.online_eval_outputs_softmax = [output_softmax.detach().cpu().numpy() for output_softmax in outputs_softmax]
+                self.online_eval_outputs_softmax = [output_softmax.detach().cpu().numpy() for output_softmax in
+                                                    outputs_softmax]
                 self.online_eval_targets = [target.detach().cpu().numpy() for target in targets]
             else:
                 num_outputs = len(outputs)
                 for it in range(num_outputs):
-                    self.online_eval_outputs[it] = np.append(self.online_eval_outputs_softmax[it],
-                                                             outputs_softmax[it].detach().cpu().numpy())
+                    self.online_eval_outputs_softmax[it] = np.append(self.online_eval_outputs_softmax[it],
+                                                                     outputs_softmax[it].detach().cpu().numpy())
                     self.online_eval_targets[it] = np.append(self.online_eval_targets[it],
                                                              targets[it].detach().cpu().numpy())
 
